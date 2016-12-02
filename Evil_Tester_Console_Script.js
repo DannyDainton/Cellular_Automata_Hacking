788e555529b48cb138e04e6c3d6e01f3c525da5d
Danny Dainton <dannydainton@gmail.com>
2016-12-02 13:22:47 +0000

    First commit

---
+++ Evil_Tester_Console_Script.js
@@ -0,0 +1,177 @@
+// Clears the invasion that has started when the page loads
+stopInvasion();
+world.nukeEmAll();
+
+//Adding all the letters using the block life form into the different points on the world canvas, to spell out "Evil Tester"
+
+//E
+
+addLifeForms.block(world, 100,50)
+addLifeForms.block(world, 105,50)
+addLifeForms.block(world, 110,50)
+addLifeForms.block(world, 115,50)
+addLifeForms.block(world, 100,55)
+addLifeForms.block(world, 100,60)
+addLifeForms.block(world, 100,65)
+addLifeForms.block(world, 105,65)
+addLifeForms.block(world, 110,65)
+addLifeForms.block(world, 115,65)
+addLifeForms.block(world, 110,65)
+addLifeForms.block(world, 100,70)
+addLifeForms.block(world, 100,75)
+addLifeForms.block(world, 100,80)
+addLifeForms.block(world, 105,80)
+addLifeForms.block(world, 110,80)
+addLifeForms.block(world, 115,80)
+
+//V
+
+addLifeForms.block(world, 125,50)
+addLifeForms.block(world, 125,55)
+addLifeForms.block(world, 125,60)
+addLifeForms.block(world, 125,65)
+addLifeForms.block(world, 125,70)
+addLifeForms.block(world, 130,75)
+addLifeForms.block(world, 135,80)
+addLifeForms.block(world, 140,75)
+addLifeForms.block(world, 145,70)
+addLifeForms.block(world, 145,65)
+addLifeForms.block(world, 145,60)
+addLifeForms.block(world, 145,55)
+addLifeForms.block(world, 145,50)
+
+//I
+
+addLifeForms.block(world, 155,50)
+addLifeForms.block(world, 155,55)
+addLifeForms.block(world, 155,60)
+addLifeForms.block(world, 155,65)
+addLifeForms.block(world, 155,70)
+addLifeForms.block(world, 155,75)
+addLifeForms.block(world, 155,80)
+
+//L
+
+addLifeForms.block(world, 165,50)
+addLifeForms.block(world, 165,55)
+addLifeForms.block(world, 165,60)
+addLifeForms.block(world, 165,65)
+addLifeForms.block(world, 165,70)
+addLifeForms.block(world, 165,75)
+addLifeForms.block(world, 165,80)
+addLifeForms.block(world, 170,80)
+addLifeForms.block(world, 175,80)
+addLifeForms.block(world, 180,80)
+
+//T
+
+addLifeForms.block(world, 190,50)
+addLifeForms.block(world, 195,50)
+addLifeForms.block(world, 200,50)
+addLifeForms.block(world, 205,50)
+addLifeForms.block(world, 210,50)
+addLifeForms.block(world, 200,55)
+addLifeForms.block(world, 200,60)
+addLifeForms.block(world, 200,65)
+addLifeForms.block(world, 200,70)
+addLifeForms.block(world, 200,75)
+addLifeForms.block(world, 200,80)
+
+//E
+
+addLifeForms.block(world, 220,50)
+addLifeForms.block(world, 225,50)
+addLifeForms.block(world, 230,50)
+addLifeForms.block(world, 235,50)
+addLifeForms.block(world, 220,55)
+addLifeForms.block(world, 220,60)
+addLifeForms.block(world, 220,65)
+addLifeForms.block(world, 225,65)
+addLifeForms.block(world, 230,65)
+addLifeForms.block(world, 235,65)
+addLifeForms.block(world, 220,70)
+addLifeForms.block(world, 220,75)
+addLifeForms.block(world, 220,80)
+addLifeForms.block(world, 225,80)
+addLifeForms.block(world, 230,80)
+addLifeForms.block(world, 235,80)
+
+//S
+
+addLifeForms.block(world, 245,50)
+addLifeForms.block(world, 250,50)
+addLifeForms.block(world, 255,50)
+addLifeForms.block(world, 260,50)
+addLifeForms.block(world, 245,55)
+addLifeForms.block(world, 245,60)
+addLifeForms.block(world, 245,65)
+addLifeForms.block(world, 250,65)
+addLifeForms.block(world, 255,65)
+addLifeForms.block(world, 260,65)
+addLifeForms.block(world, 260,70)
+addLifeForms.block(world, 260,75)
+addLifeForms.block(world, 260,80)
+addLifeForms.block(world, 255,80)
+addLifeForms.block(world, 250,80)
+addLifeForms.block(world, 245,80)
+
+//T
+
+addLifeForms.block(world, 270,50)
+addLifeForms.block(world, 275,50)
+addLifeForms.block(world, 280,50)
+addLifeForms.block(world, 285,50)
+addLifeForms.block(world, 290,50)
+addLifeForms.block(world, 280,55)
+addLifeForms.block(world, 280,60)
+addLifeForms.block(world, 280,65)
+addLifeForms.block(world, 280,70)
+addLifeForms.block(world, 280,75)
+addLifeForms.block(world, 280,80)
+
+//E
+
+addLifeForms.block(world, 300,50)
+addLifeForms.block(world, 305,50)
+addLifeForms.block(world, 310,50)
+addLifeForms.block(world, 315,50)
+addLifeForms.block(world, 300,55)
+addLifeForms.block(world, 300,60)
+addLifeForms.block(world, 300,65)
+addLifeForms.block(world, 305,65)
+addLifeForms.block(world, 310,65)
+addLifeForms.block(world, 315,65)
+addLifeForms.block(world, 300,70)
+addLifeForms.block(world, 300,75)
+addLifeForms.block(world, 300,80)
+addLifeForms.block(world, 305,80)
+addLifeForms.block(world, 310,80)
+addLifeForms.block(world, 315,80)
+
+//R
+
+addLifeForms.block(world, 325,50)
+addLifeForms.block(world, 330,50)
+addLifeForms.block(world, 335,50)
+addLifeForms.block(world, 340,50)
+addLifeForms.block(world, 340,50)
+addLifeForms.block(world, 340,55)
+addLifeForms.block(world, 340,60)
+addLifeForms.block(world, 340,65)
+addLifeForms.block(world, 335,65)
+addLifeForms.block(world, 330,65)
+addLifeForms.block(world, 325,55)
+addLifeForms.block(world, 325,60)
+addLifeForms.block(world, 325,65)
+addLifeForms.block(world, 325,70)
+addLifeForms.block(world, 325,75)
+addLifeForms.block(world, 325,80)
+addLifeForms.block(world, 330,70)
+addLifeForms.block(world, 335,75)
+addLifeForms.block(world, 340,80)
+
+// Add Alan's colour changing code from https://www.youtube.com/watch?v=hfE99d4Slck&index=2&list=PLrM2ELpRbbu58T2IDqEyJ9oK98xGu4r9V
+var colourChangeBot = setInterval(function() {
+    world.entityColour = "#" +
+    (("000000" + getRandomInt(0, 0xFFFFFF).toString(16)).substr(-6));
+}, 1000);
